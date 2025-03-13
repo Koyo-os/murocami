e3eb99d98c3e48c59168e801f423ca084ef2b69c
osamikoyo <v.pylayev@mail.ru>
2025-03-13 12:43:02 +0500

    update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,10 @@
 
 YAML_FILE="config.yaml"
 
+OUTPUT="bin/app"
+
+INPUT="cmd/main.go"
+
 if [[ ! -f "$YAML_FILE" ]]; then
   echo "Файл $YAML_FILE не найден."
   exit 1
@@ -11,11 +15,11 @@ USE_UI=$(yq e '.use_ui' "$YAML_FILE")
 
 if [[ "$USE_UI" == "true" ]]; then
     ./scripts/build_ui.sh
-    make build
-    ./bin/app
+    go build -o $OUTPUT $INPUT
+    $OUTPUT
     exit 0
 else
-  make build
-  ./bin/app 
-  exit 0
+    go build -o $OUTPUT $INPUT
+    $OUTPUT
+    exit 0
 fi
